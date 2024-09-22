3ad9d25f5f63acd621c2e7001815a92ca3377a63
ThiagoCoser <thiago.coser@gmail.com>
2024-09-22 20:49:06 -0300

    update

--- 09.Files.py
+++ 09.Files.py
@@ -14,12 +14,49 @@ print (type(meuArquivo))
 with open('teste.txt', 'r') as reader:
     print(reader.read())
 
-#%%
 with open('teste.txt', 'r') as reader:
-    print(reader.readline(10))
+
+    print(reader.readline())
+    print(reader.readline())
+    print(reader.readline())
 
 with open('teste.txt', 'r') as reader:
-    print(reader.readlines(5))
+
+    lista = reader.readlines()
+    print(lista)
+    print(lista[3])
+
+# Métodos
+# .read():
+# Lê todo o conteúdo do arquivo de uma vez como uma única string.
+
+#. readline():
+# Lê uma linha de cada vez do arquivo e retorna como string, incluindo o caractere de nova linha \n, se houver.
+# Melhor utilizado em grandes arquivos e volumes de dados, logs, datasets, etc
+
+# .readlines()
+# Lê todas as linhas do arquivo e retorna uma lista, onde cada elemento é uma linha do arquivo.
+
+# Controle de fluxo
+
+# Imprime todas as linhas
+with open('teste.txt', 'r') as reader:
+    for linha in reader:
+        # Processa cada linha individualmente
+        print(linha)
+
+# Procura uma palavra-chave em cada linha
+with open('teste.txt', 'r') as reader:
+    while True:
+        linha = reader.readline()
+        if not linha:  # Fim do arquivo
+            break
+        if "teste" in linha:
+            print(f"Palavra encontrada: {linha}")
+            break
+        else:
+            print(f"Palavra não encontrada")
+
 
 #%%
 # Escrevendo um novo arquivo olaMundo
@@ -30,3 +67,18 @@ file.write("Olá mundo!")
 
 with open('olaMundo.txt', 'r') as reader:
     print(reader.read())
+
+# Extra, abrindo arquivos de url
+
+# pip install requests
+
+import requests
+
+url = "https://raw.githubusercontent.com/ThiagoCoser/cursoPython/main/teste.txt"
+
+dadoOnline= requests.get(url)
+
+
+meuArquivo = dadoOnline.text
+print(type(meuArquivo))  # Agora será uma string
+print(meuArquivo)        # Conteúdo do arquivo
